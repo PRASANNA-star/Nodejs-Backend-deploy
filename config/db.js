f1b77c1164a95ceac35a40938549f885b5a006f2
Prasanna_K_M <prasanna.marimuthu@focusrtech.com>
2025-05-13 15:37:40 +0530

    Gowsh

--- config/db.js
+++ config/db.js
@@ -1,11 +1,15 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-    connectionLimit: 10,  // max connections in pool
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'nodejscrud'
+    connectionLimit: 10,
+    host: process.env.DB_HOST,       // Replace with actual values for local testing
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT || 3306,  // Optional
+    ssl: {
+        rejectUnauthorized: false     // Needed for services like PlanetScale
+    }
 });
 
 module.exports = pool;

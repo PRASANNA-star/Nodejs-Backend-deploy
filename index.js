2c8772e693fee24cc82643824c934d9219669c8b
Prasanna_K_M <prasanna.marimuthu@focusrtech.com>
2025-05-13 15:42:31 +0530

    menan

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const userRoutes = require('./routes/userRoutes');
 const PORT = process.env.PORT ;
+
 console.log("PORt",PORT)
 const app = express();
 
